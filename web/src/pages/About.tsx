321cfcdb08f75330e02711c4a7619330a9f7edaa
Hillay Koon <iace@foxmail.com>
2024-07-07 19:34:18 +0800

    Update About.tsx

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -9,21 +9,21 @@ const About = () => {
       <div className="w-full px-4 sm:px-6">
         <div className="w-full shadow flex flex-col justify-start items-start p-4 rounded-xl bg-white dark:bg-zinc-800 text-black dark:text-gray-300">
           <p className="text-base font-semibold">
-            Welcome to the Linkin Love Bulletin Board!
+            💭Welcome to the Linkin Love Bulletin Board!💭
           </p>
           <p className="text-base font-regular">Here, you'll find regular updates on our development progress.</p>
           <p className="text-base font-regular">We encourage everyone to join the conversation and share your thoughts!</p>
-        </div>
-        <div className="mt-1 flex flex-row items-center flex-wrap">
-          <Link underline="always" href="https://discord.gg/gXR53sWP" target="_blank">
-              Discord
-          </Link>
-          <Icon.Dot className="w-4 h-auto opacity-60" />
-          <Link underline="always" href="https://linkin.love/" target="_blank">
-              Website
-          </Link>
-          <Icon.Dot className="w-4 h-auto opacity-60" />
-          <p className="text-base font-regular">Powered by Memos</p>
+          <div className="mt-1 flex flex-row items-center flex-wrap">
+            <Link underline="always" href="https://discord.gg/gXR53sWP" target="_blank">
+                Discord
+            </Link>
+            <Icon.Dot className="w-4 h-auto opacity-60" />
+            <Link underline="always" href="https://linkin.love/" target="_blank">
+                Website
+            </Link>
+            <Icon.Dot className="w-4 h-auto opacity-60" />
+            <p className="text-base font-regular">Powered by Memos</p>
+          </div>
         </div>
       </div>
     </section>

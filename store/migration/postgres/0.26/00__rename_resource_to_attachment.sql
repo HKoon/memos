e2b5793bed5e40b1a8d94794b20cd28cb96b64ef
Hillay Koon <iace@foxmail.com>
2026-02-18 03:35:55 +0800

    fix(store): 安全重命名 resource 表为 attachment
    添加条件判断，仅当 resource 表存在且 attachment 表不存在时才执行重命名操作，避免在重复执行迁移脚本时导致错误。

--- store/migration/postgres/0.26/00__rename_resource_to_attachment.sql
+++ store/migration/postgres/0.26/00__rename_resource_to_attachment.sql
@@ -1,1 +1,7 @@
-ALTER TABLE resource RENAME TO attachment;
+DO $$
+BEGIN
+  IF EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = current_schema() AND table_name = 'resource') 
+  AND NOT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = current_schema() AND table_name = 'attachment') THEN
+    ALTER TABLE resource RENAME TO attachment;
+  END IF;
+END $$;
